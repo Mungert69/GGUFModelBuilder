a2378fe57d95d9c6f811659b5807722777b36faf
Mungert69 <contact@mahadeva.co.uk>
2023-01-15 23:27:41 +0000

    removed empty lines

--- Services/IMonitorPingProcessor.cs
+++ Services/IMonitorPingProcessor.cs
@@ -4,23 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using NetworkMonitor.Objects;
 using NetworkMonitor.Objects.ServiceMessage;
-
 namespace NetworkMonitor.Processor.Services
 {
     public interface IMonitorPingProcessor
     {
         void init(ProcessorInitObj initObj);
-
         void AddMonitorIPsToQueueDic(ProcessorQueueDicObj queueObj);
         ResultObj Connect(ProcessorConnectObj connectObj);
-
         List<ResultObj> UpdateAlertSent(List<int> monitorPingInfoIDs, bool alertSent);
         List<ResultObj> UpdateAlertFlag(List<int> monitorPingInfoIDs, bool alertFlag);
-
         ResultObj ResetAlert(int monitorPingInfoID);
         void AddRemovePingInfos(List<RemovePingInfo> removePingInfos);
-
         bool Awake{get;set;}
-       
     }
 }
