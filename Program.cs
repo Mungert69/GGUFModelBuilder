12c33259907b25c27eaeb68cbf7a42ec4cb5cade
Mungert69 <contact@mahadeva.co.uk>
2023-02-07 16:42:54 +0000

    removed dapr from Processor

--- Program.cs
+++ Program.cs
@@ -1,33 +1,32 @@
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using CloudNative.CloudEvents;
+using CloudNative.CloudEvents.NewtonsoftJson;
+using Newtonsoft.Json.Linq;
+using NetworkMonitor.Objects.ServiceMessage;
+using NetworkMonitor.Objects.Repository;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using NetworkMonitor.Processor.Services;
+using NetworkMonitor.Connection;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Net;
 
-namespace NetworkMonitor.Processor
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+IConfiguration config = new ConfigurationBuilder()
+   .AddJsonFile("/home/mahadeva/code/RabbitMQTest/Receive/appsettings.json", optional: false, reloadOnChange: true)
+   .AddEnvironmentVariables()
+   .AddCommandLine(args)
+   .Build();
+var loggerFactory = LoggerFactory.Create(builder =>
         {
-            IConfigurationRoot config = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", optional: false)
-        .Build();
-            IWebHost host = CreateWebHostBuilder(args).Build();
-            host.Run();
-        }
+            builder
+                .AddFilter("Microsoft", LogLevel.Warning)
+                .AddFilter("System", LogLevel.Warning)
+                .AddFilter("LoggingConsoleApp.Program", LogLevel.Debug)
+                .AddConsole();
+        });
+ILogger<MonitorPingProcessor> logger = loggerFactory.CreateLogger<MonitorPingProcessor>();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-
-
-        WebHost.CreateDefaultBuilder(args).UseKestrel(options =>
-    {
-        //options.Listen(IPAddress.Loopback, 5000);  // http:localhost:5000
-        options.Listen(IPAddress.Any, 2054);         // http:*:65123
-        options.Listen(IPAddress.Any, 2055);
-    }).UseStartup<Startup>();
-    }
-}
+var connectFactory = new ConnectFactory();
+var _monitorPingProcessor = new MonitorPingProcessor(config, logger, connectFactory);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
