bd627ff867fbc78aa8ca34373f87551cb610247b
Mungert69 <mungert@gmail.com>
2023-03-28 00:22:46 +0100

    added Host and Instance name from config

--- Services/MonitorPingProcessor.cs
+++ Services/MonitorPingProcessor.cs
@@ -46,9 +46,8 @@ namespace NetworkMonitor.Processor.Services
             //_daprClient = daprClient;
             // Special case 2min timeout for large published messages.
             _appID = config.GetValue<string>("AppID");
-            string instanceName = config.GetValue<string>("InstanceName");
-            //string hostName=config.GetValue<string>("hostname");
-            string hostName = "rabbitmq";
+            string instanceName = config.GetValue<string>("RabbitInstanceName");
+            string hostName=config.GetValue<string>("RabbitHostName");
             _logger.Info(" Starting Processor with AppID = " + _appID + " instanceName=" + instanceName + " connecting to RabbitMQ at " + hostName);
             _connectFactory = connectFactory;
             _rabbitRepo = new RabbitListener(_logger, this, _appID, instanceName, hostName);
