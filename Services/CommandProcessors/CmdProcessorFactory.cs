7f7cc7213a7b2cb02f340dc733564092b719158f
Mungert69 <contact@mahadeva.co.uk>
2024-09-27 23:56:11 +0100

    add ping cmd processor

--- Services/CommandProcessors/CmdProcessorFactory.cs
+++ Services/CommandProcessors/CmdProcessorFactory.cs
@@ -14,6 +14,7 @@ namespace NetworkMonitor.Processor.Services
         ICmdProcessor GetBusyboxProcessor();
         ICmdProcessor GetSearchWebProcessor();
         ICmdProcessor GetCrawlPageProcessor();
+        ICmdProcessor GetPingProcessor();
         ILocalCmdProcessorStates NmapStates { get; }
 
         ILocalCmdProcessorStates MetasploitStates { get; }
@@ -22,6 +23,7 @@ namespace NetworkMonitor.Processor.Services
         ILocalCmdProcessorStates BusyboxStates { get; }
         ILocalCmdProcessorStates SearchWebStates { get; }
         ILocalCmdProcessorStates CrawlPageStates { get; }
+        ILocalCmdProcessorStates PingStates { get; }
     }
 
     public class CmdProcessorFactory : ICmdProcessorProvider
@@ -36,6 +38,7 @@ namespace NetworkMonitor.Processor.Services
         private readonly ILocalCmdProcessorStates _busyboxStates;
         private readonly ILocalCmdProcessorStates _searchwebStates;
         private readonly ILocalCmdProcessorStates _crawlpageStates;
+        private readonly ILocalCmdProcessorStates _pingStates;
 
         private ICmdProcessor _nmapProcessor;
         private ICmdProcessor _metasploitProcessor;
@@ -43,15 +46,15 @@ namespace NetworkMonitor.Processor.Services
         private ICmdProcessor _busyboxProcessor;
         private ICmdProcessor _searchwebProcessor;
         private ICmdProcessor _crawlpageProcessor;
+        private ICmdProcessor _pingProcessor;
 
         public ILocalCmdProcessorStates NmapStates => _nmapStates;
-
         public ILocalCmdProcessorStates MetasploitStates => _metasploitStates;
-
         public ILocalCmdProcessorStates OpensslStates => _opensslStates;
         public ILocalCmdProcessorStates BusyboxStates => _busyboxStates;
         public ILocalCmdProcessorStates SearchWebStates => _searchwebStates;
         public ILocalCmdProcessorStates CrawlPageStates => _crawlpageStates;
+        public ILocalCmdProcessorStates PingStates => _pingStates;
 
         public CmdProcessorFactory(ILoggerFactory loggerFactory, IRabbitRepo rabbitRepo, NetConnectConfig netConfig)
             : this(loggerFactory, rabbitRepo, netConfig,
@@ -60,7 +63,8 @@ namespace NetworkMonitor.Processor.Services
                    new LocalOpensslCmdProcessorStates(),
                    new LocalBusyboxCmdProcessorStates(),
                    new LocalSearchWebCmdProcessorStates(),
-                   new LocalCrawlPageCmdProcessorStates()
+                   new LocalCrawlPageCmdProcessorStates(),
+                   new LocalPingCmdProcessorStates()
                    )
         {
 
@@ -74,7 +78,8 @@ namespace NetworkMonitor.Processor.Services
             ILocalCmdProcessorStates opensslStates,
             ILocalCmdProcessorStates busyboxStates,
             ILocalCmdProcessorStates searchwebStates,
-            ILocalCmdProcessorStates crawlpageStates
+            ILocalCmdProcessorStates crawlpageStates,
+               ILocalCmdProcessorStates pingStates
             )
         {
             _loggerFactory = loggerFactory;
@@ -86,6 +91,7 @@ namespace NetworkMonitor.Processor.Services
             _busyboxStates = busyboxStates;
             _searchwebStates = searchwebStates;
             _crawlpageStates = crawlpageStates;
+            _pingStates = pingStates;
 
             // Create processors
             _nmapProcessor = CreateProcessor("nmap");
@@ -94,6 +100,7 @@ namespace NetworkMonitor.Processor.Services
             _busyboxProcessor = CreateProcessor("busybox");
             _searchwebProcessor = CreateProcessor("searchweb");
             _crawlpageProcessor = CreateProcessor("crawlpage");
+            _pingProcessor = CreateProcessor("ping");
         }
 
         public ICmdProcessor GetNmapProcessor() => _nmapProcessor;
@@ -102,6 +109,7 @@ namespace NetworkMonitor.Processor.Services
         public ICmdProcessor GetBusyboxProcessor() => _busyboxProcessor;
         public ICmdProcessor GetSearchWebProcessor() => _searchwebProcessor;
         public ICmdProcessor GetCrawlPageProcessor() => _crawlpageProcessor;
+        public ICmdProcessor GetPingProcessor() => _pingProcessor;
 
 
         private ICmdProcessor CreateProcessor(string processorType)
@@ -152,6 +160,12 @@ namespace NetworkMonitor.Processor.Services
                         _rabbitRepo,
                         _netConfig
                     );
+                case "ping":
+                    return new PingCmdProcessor(
+                        _loggerFactory.CreateLogger<PingCmdProcessor>(),
+                        PingStates,
+                        _rabbitRepo,
+                        _netConfig);
 
                 default:
                     throw new ArgumentException($"Unsupported processor type: {processorType}");
